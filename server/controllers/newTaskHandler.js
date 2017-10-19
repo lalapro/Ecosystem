82d4b17211172fa542437910e0df8a187e4344ed
Kaitlin Blacketor <kaitlinblacketor@Kaitlins-MacBook-Pro.local>
2017-10-19 19:19:08 -0400

    refactor

--- server/controllers/newTaskHandler.js
+++ server/controllers/newTaskHandler.js
@@ -11,37 +11,18 @@ const handlenNewTask = (req, res) => {
   let Frequency = req.body.frequency;
   let Days = req.body.days;
   //pull category_ID and marker_ID
-  let Category = req.body.category;
-  let Marker = req.body.location;
+  let Category_ID = req.body.category;
+  let Marker_ID = req.body.location;
   let User_ID = req.body.userID;
-
-  let selectCategory = `SELECT ID FROM CategoryDeets WHERE Category = '${Category}'`;
-  let selectMarker = `SELECT Marker_ID FROM Marker WHERE Marker_Title = '${Marker}'`;
-
-  db.query(selectCategory, null, (err, results) => {
+  let insert = `INSERT INTO Tasks (Task_ID, Task_Title, Task_Description, Completion, Start, End, Frequency, Days, User_ID, Category_ID, Marker_ID) VALUES (null, '${Title}', '${Description}', null, '${Start}', '${End}', '${Frequency}', '${Days}', '${User_ID}', '${Category_ID}', '${Marker_ID}')`
+  db.query(insert, null, (err, results) => {
     if (err) {
-      res.status(404).send(`We encountered an error looking up the category ${err}`);
+      res.status(404).send(`We encountered an error creating the task ${err}`);
     } else {
-      let Category_ID;
-      results[0] ? Category_ID = results[0].ID : Category_ID = 'undefined';
-      db.query(selectMarker, null, (err, results) => {
-        if (err) {
-          res.status(404).send(`We encountered an error looking up the location ${err}`);
-        } else {
-          let Marker_ID;
-          results[0] ? Marker_ID = results[0].Marker_ID : Marker_ID = 'undefined';
-          let insert = `INSERT INTO Tasks (Task_ID, Task_Title, Task_Description, Completion, Start, End, Frequency, Days, User_ID, Category_ID, Marker_ID) VALUES (null, '${Title}', '${Description}', null, '${Start}', '${End}', '${Frequency}', '${Days}', '${User_ID}', '${Category_ID}', '${Marker_ID}')`
-          db.query(insert, null, (err, results) => {
-            if (err) {
-              res.status(404).send(`We encountered an error creating the task ${err}`);
-            } else {
-              res.status(201).send(results);
-            }
-          })
-        }
-      })
+      res.status(201).send(results);
     }
   })
+
 }
 
 
